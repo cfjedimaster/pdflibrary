6117eaa4c7f71f5aaf9df2857942e4e002cfb56c
Raymond Camden <raymondcamden@gmail.com>
2021-09-09 14:58:27 -0500

    changes

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 const pdfLib = require('./');
 
+/*
+Requires a file called samples/hamlet.pdf, change to a PDF on your system.
+*/
 (async () => {
 
 	const lib = new pdfLib('./pdftools-api-credentials.json');
 
 	try {
-		await lib.export('./hamlet.pdf', './output/test.pptx', true);
+		await lib.export('./samples/hamlet.pdf', './samples/test.pptx', true);
 	} catch(e) {
 		console.error(e);
 	}
